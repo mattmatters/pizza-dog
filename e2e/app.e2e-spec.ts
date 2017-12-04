5e60abb1041a3c913d99cca5f5c4d581865bffd7
Matt Lewis <domattthings@gmail.com>
2017-12-04 18:47:07 -0500

    PhantomJS for e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('thing App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to Wiener Pizza!');
   });
 });
