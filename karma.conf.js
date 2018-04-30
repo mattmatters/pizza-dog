9f43c5f66f3223db51430afc5580edc65d956cfc
Matt Lewis <domattthings@gmail.com>
2018-04-29 21:05:16 -0400

    Specify coverage output path

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const path = require('path');
 
 module.exports = function (config) {
   config.set({
@@ -17,7 +18,8 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      dir: path.join(__dirname, 'coverage')
     },
     angularCli: {
       environment: 'dev'
